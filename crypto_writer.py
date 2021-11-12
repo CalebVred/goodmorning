6a532fdf73f77c85b9891bba2a9f5ee645d54088
Caleb Vredevoogd <cvredevoogd99@gmail.com>
2021-11-12 10:36:02 -0500

    Adds documentation

--- crypto_writer.py
+++ crypto_writer.py
@@ -4,6 +4,11 @@ Returns a string giving cryptocurrency stats from the last 24 hours
 from pycoingecko import CoinGeckoAPI
 
 
+'''
+@Function: crypto_stat_string(coin_list, vsc)
+@Input: coin_list - list of strings representing cryptocurrencies on CoinGecko, vsc - abbreviated string representing currency used to gauge crypto price
+@Output: c_stat_str - string of cryptocurrency statistics
+'''
 def crypto_stat_string(coin_list, vsc):
     c_stat_str = ""
     cg = CoinGeckoAPI()
@@ -11,7 +16,7 @@ def crypto_stat_string(coin_list, vsc):
     for coin in coin_list:
         print("Appending stats for", coin)
         c_stat_str += coin
-        c_stat_str += " is priced at "
+        c_stat_str += " is currently priced at "
         cgdatac = cg.get_price(ids=coin, vs_currencies=vsc)
         cid = cgdatac[coin]
         price = cid[vsc]
